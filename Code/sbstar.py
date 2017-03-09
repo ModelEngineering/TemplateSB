050dcc90237b895ec847d2ae62e26775cc9d0c93
Joseph Hellerstein <jlheller@uw.edu>
2017-03-09 11:24:00 -0800

    Initial code.

---
+++ Code/sbstar.py
@@ -0,0 +1,4 @@
+'''Class for processing templates. See README for syntax.'''
+
+class Sbstar(object):
+  pass
---
+++ Code/test_sbstar.py
@@ -0,0 +1,18 @@
+import unittest
+from sbstar import Sbstar
+
+
+IGNORE_TEST = False
+
+
+#############################
+# Tests
+#############################
+# pylint: disable=W0212,C0111,R0904
+class TestSbstar(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+if __name__ == '__main__':
+  unittest.main()
