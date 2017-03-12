7c82bebe72b63db29fb8b4c219681c23d698cff9
Joseph Hellerstein <jlheller@uw.edu>
2017-03-12 15:16:19 -0700

    Python script to run SbStar.

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
 """
    Running the template pre-processor standalone.
-   Input: Templated Antimony model (standard in)
-   Output: Expanded Antimony model (standard out)
+   Input: Templated Antimony model (stdin)
+   Output: Expanded Antimony model (stdout)
 """
-raise RuntimeError("Not yet implemented.")
+import fileinput
+import sys
+from Code.sbstar import SbStar
+
+template_stg = ''
+for line in fileinput.input():
+  template_stg += "\n" + line
+
+sbstar = SbStar(template_stg)
+expanded_stg = sbstar.expand()
+
+sys.stdout.write(expanded_stg)
