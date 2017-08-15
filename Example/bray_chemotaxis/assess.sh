dffd5adbf335beb7f1ad98aed06a981ff4a118b0
Joseph Hellerstein <jlheller@uw.edu>
2017-08-15 16:02:34 -0700

    Evaluation of TemplateSB output with original model

---
+++ Example/bray_chemotaxis/assess.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Compare the original file with the model file
+
+function filter {
+  this_file=$1
+  grep -v  "^ *\/" ${this_file}   \
+    | grep -v "^ *#"              \
+    | grep -v "^ *$"              \
+    | sed 's/^ *//'               \
+    > /tmp/${this_file}
+}  
+
+original=`ls BIOM*.mdl`
+processed_name="bray_model"
+processed=`ls ${processed_name}.mdl`
+python ../../run.py < ${processed_name}.tmpl > ${processed}
+filter $original
+filter $processed
+diff /tmp/${original} /tmp/${processed} > /tmp/assess.out
+cat /tmp/assess.out
