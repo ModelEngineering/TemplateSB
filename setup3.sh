5cf9b9e0bc975a8b8e767ba307fc6dfa2e0232a3
Joseph Hellerstein <jlheller@uw.edu>
2017-10-25 09:07:19 -0700

    Install script for python 3

---
+++ setup3.sh
@@ -0,0 +1,8 @@
+# These installs are
+# required to run the demo
+# Note that you must have conda (or miniconda) installed to 
+# run this script.
+sudo pip3 install tellurium
+sudo pip3 install python-libsbml
+# Simple YAML
+sudo pip3 install dowright
