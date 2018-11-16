a22121c28353963e968299c89e288bf05d30c0be
Joe Hellerstein <jlheller@uw.edu>
2018-11-15 16:43:52 -0800

    Updated setup for python3

--- setup3.sh
+++ setup3.sh
@@ -1,7 +1,6 @@
 # These installs are
 # required to run the demo using python 3.
-sudo pip3 install tellurium
-sudo pip3 install python-libsbml
-sudo apt-get install python3-tk
+sudo pip install tellurium
+#sudo apt-get install python3-tk
 # Simple YAML
-sudo pip3 install dowright
+sudo $HOME/miniconda3/bin/conda install pyyaml
